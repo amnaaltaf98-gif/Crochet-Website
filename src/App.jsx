e11885c84a005398b23b213f9f62a79bd04d0c68
Amna Altaf <amnaaltaf98@gmail.com>
2026-06-24 21:09:57 +0500

    First website

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <div>
+      <h1>Amna's Crochet Shop</h1>
+
+      <h2>Handmade Crochet Creations</h2>
+
+      <p>
+        Welcome to my crochet business.
+        I create handmade flowers,
+        plushies, keychains and custom orders.
+      </p>
+
+      <h2>Products</h2>
+
+      <ul>
+        <li>Crochet Flowers</li>
+        <li>Plushies</li>
+        <li>Keychains</li>
+      </ul>
+
+      <button>
+        Order on WhatsApp
+      </button>
+    </div>
+  );
+}
+
+export default App;
